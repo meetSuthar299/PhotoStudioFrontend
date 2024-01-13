528cb3f524bdcbd7b2d8bdaefe4d0cf7391d53f7
Meet Suthar <meetsuthar299@gmail.com>
2024-01-12 21:56:19 -0700

    hero page video update

--- src/Pages/HeroPage/Hero.js
+++ src/Pages/HeroPage/Hero.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import Logo, { LogoStudio } from "../../Components/Logo"
+import { LogoStudio } from "../../Components/Logo"
+import video from "../../img/gallery/forWebsite.mp4"
 
 function Hero() {
     return (
     <div id="home" className="h-screen relative bg-slate-500 overflow-hidden">
         <video
-            src='https://res.cloudinary.com/ddp8ln1ts/video/upload/v1704795384/mountain_-_34608_1440p_lz5dzb.mp4'
+            src={video}
             autoPlay muted loop
             className="w-full h-full object-cover absolute top-0 left-0 z-0"
         />
