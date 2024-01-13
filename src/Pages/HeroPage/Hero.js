331de694095baf29465ba6eaf01bd4575d920134
Meet Suthar <meetsuthar299@gmail.com>
2024-01-13 02:36:04 -0700

    Update Hero.js

--- src/Pages/HeroPage/Hero.js
+++ src/Pages/HeroPage/Hero.js
@@ -7,7 +7,9 @@ function Hero() {
     <div id="home" className="h-screen relative bg-slate-500 overflow-hidden">
         <video
             src={video}
-            autoPlay muted loop
+            muted
+            loop
+            autoPlay 
             className="w-full h-full object-cover absolute top-0 left-0 z-0"
         />
         <div className="text-white overlay absolute top-0 left-0 w-full h-full flex justify-center items-center flex-col z-1">
