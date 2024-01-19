50235c923bd67f9d1e17cb915d20839aa45754c4
Meet Suthar <meetsuthar299@gmail.com>
2024-01-18 23:16:38 -0700

    Update ServiceCard.js

--- src/Components/ServiceCard.js
+++ src/Components/ServiceCard.js
@@ -1,24 +1,33 @@
-import React from 'react'
+import React from 'react';
 
-function ServiceCard({ img, title, disc }) {
-
+function ServiceCard({ img, title, disc, offerings }) {
   return (
-    <div id={title} className="m-2 text-center relative overflow-hidden transition duration-200 transform hover:-translate-y-2 cursor-pointer lg:h-[70vh] md:h-[40vh]">
+    <div
+      id={title}
+      className="m-2 text-center relative overflow-hidden transition duration-200 transform hover:-translate-y-2 cursor-pointer lg:h-[70vh] md:h-[40vh] rounded-md shadow-md overflow-hidden border border-gray-300"
+    >
       <img
         loading="lazy"
-        className="object-cover w-full h-full"
+        className="object-cover w-full h-full rounded-md"
         src={img}
         alt={title}
         decoding="async"
       />
-      <div className="absolute inset-0 flex flex-col justify-center items-center px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-50 opacity-100">
-        <h1 className="mb-4 text-3xl text-gray-100">{title}</h1>
+      <div className="absolute inset-0 flex flex-col justify-center items-center px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-50 opacity-0 hover:opacity-100">
+        <h1 className="mb-4 text-2xl lg:text-3xl text-white font-semibold">{title}</h1>
       </div>
-      <div className="absolute inset-0 flex flex-col justify-center items-center px-6 py-4 transition-opacity duration-200 bg-[rgb(0,0,0)] opacity-0 hover:opacity-100 text-center">
-        <p className="text-lg tracking-wide text-gray-300 font-light">{disc}</p>
+      <div className="absolute inset-0 flex flex-col justify-center items-center px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-70 opacity-0 hover:opacity-100 text-center">
+        {/* <p className="text-lg text-gray-300 font-light mb-4">{disc}</p> */}
+        <ul className="text-gray-400">
+          {offerings.map((offering, index) => (
+            <li key={index} className="py-2 border-b border-gray-300">
+              <h1 className="text-lg font-thin">{offering}</h1>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
-  )
+  );
 }
 
-export default ServiceCard
+export default ServiceCard;
